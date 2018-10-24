e99b4f5fdae8300dc367db343f9991c3581d6be6
1j <1j@iMac-de-administrador-3.local>
2018-10-23 19:40:37 -0500

    Lienzo actividad

--- EjemploFor/ejemplofor.js
+++ EjemploFor/ejemplofor.js
@@ -2,16 +2,19 @@ var btnUno = document.querySelector('#btnUno');
 var btnDos = document.querySelector('#btnDos');
 var btnTres = document.querySelector('#btnTres');
 var btnCuatro = document.querySelector('#btnCuatro');
+var btnCinco = document.querySelector('#btnCinco');
 
 var opcion = document.querySelector('#opciones');
 var inputNumero = document.querySelector('#numero');
 var resultado = document.querySelector('#resultado');
+var lienzo = document.querySelector('#lienzo');
 
 
 btnUno.addEventListener('click',ejemplo1);
 btnDos.addEventListener('click',ejemplo2);
 btnTres.addEventListener('click',ejemplo3);
 btnCuatro.addEventListener('click',ejemplo4);
+btnCinco.addEventListener('click',ejemplo5);
 
 function ejemplo1(){ //Ciclo for
 
@@ -56,4 +59,27 @@ function ejemplo4(){
         i--;
     }
 
+}
+function ejemplo5(){
+    var c = lienzo.getContext('2d');
+    var i = 0;
+    
+    do{
+        var x = Math.trunc((Math.random()*501)+1);
+        var y = Math.trunc((Math.random()*501)+1);
+        var r = Math.trunc((Math.random()*50)+1);
+
+        if(x>250){
+            break;
+        }
+
+        c.beginPath();
+        c.fillStyle = 'rgba(0,255,0,0.1)';
+        c.arc(x,y,r,0, 2*Math.PI);
+        c.fill();
+        i++;
+    }while(i<30);
+    
+    
+    
 }
